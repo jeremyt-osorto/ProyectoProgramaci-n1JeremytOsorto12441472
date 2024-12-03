80e9101b0aabad9faa3545a4766d705c5bc8d4c7
ukken kun <jeremyt_osorto@unitec.edu>
2024-12-02 18:12:22 -0600

    First commit

---
+++ src/proyectoprogramacion1jeremytosorto12441472/ProyectoProgramacion1JeremytOsorto12441472.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyectoprogramacion1jeremytosorto12441472;
+
+/**
+ *
+ * @author DELL
+ */
+public class ProyectoProgramacion1JeremytOsorto12441472 {
+    static GUIproyecto pro=new GUIproyecto();
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        pro.setVisible(true);
+    }
+    
+}
